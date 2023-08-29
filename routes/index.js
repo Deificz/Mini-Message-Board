d340f053f1eee0fe6df16b50c633770debe94f04
Deificz <99456747+Deificz@users.noreply.github.com>
2023-08-29 20:42:56 +0800

    Finished project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+   {
+     text: "Hi there!",
+     user: "Aeron",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Brylle",
+     added: new Date()
+   }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { messages: messages });
+});
+
+router.get('/', function(req, res, next) {
+  res.redirect('/new')
+});
+
+router.post('/new', function(req,res,next){
+  const userName = req.body.userName;
+  const userMessage = req.body.userMessage;
+  messages.push({text: userMessage, user: userName, added: new Date()});
+  res.redirect('/');
 });
 
 module.exports = router;
--- routes/new.js
+++ routes/new.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('New');
+  res.render('form',{})
 });
+
 
 module.exports = router;
